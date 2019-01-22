b1f82de38b062a137b1890b67e08ce9f8796d8b2
Peter Szabo <peter.szabo@agoda.com>
2019-01-22 11:11:34 +0700

    Initial commit

---
+++ HttpClientInvalidHeaders.Test/TestWithWebAppFactory.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HttpClientInvalidHeaders.Test
+{
+    public class TestWithWebAppFactory : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public TestWithWebAppFactory(WebApplicationFactory<Startup> webApplicationFactory)
+        {
+            _client = webApplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("/api/values", UriKind.Relative)
+            };
+            request.Headers.TryAddWithoutValidation("User-Agent", "UserAgent: {K: V}");
+            request.Content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+
+            Assert.True(response.IsSuccessStatusCode);
+        }
+    }
+}
